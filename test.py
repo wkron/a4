021095bb8f5318e1308f58c6fce655ad9c8813cc
Mark - MakkerW <welinmark@gmail.com>
2023-01-04 14:04:18 +0100

    working gpt implementation

--- test.py
+++ test.py
@@ -1,52 +1,66 @@
 import socket
 import os
-import re
+import mimetypes
 
-# Create a socket and bind it to the specified port
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('', 8000))
+# Constants
+HOST = ''
+PORT = 8000
+BUFFER_SIZE = 1024
 
-# Set the root folder for the server
-root_folder = ''
+# Create the server socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
 
-# Listen for incoming connections
-sock.listen(1)
+print(f'Listening for connections on port {PORT}')
 
+# Run the server indefinitely
 while True:
-    # Accept a connection
-    conn, addr = sock.accept()
-    print('Connected by', addr)
-    # Receive the request
-    request = conn.recv(1024).decode()
-    # Split the request into individual lines
-    lines = request.split('\n')
-    # Extract the request method, URL, and headers
-    if len(lines) >= 3:
-     method, url, headers = lines[0], lines[1], lines[2:]
-    else:
-     method = ''
-     url = ''
-     headers = []
-
-    # Extract the root and file path from the URL
-    if '/' in url:
-     root, file_path = url.split('/', 1)
-    else:
-     root = ''
-     file_path = url
-
-    # Concatenate the root folder and file path
-    file_path = os.path.join(root_folder, file_path)
-    # Check if the file exists
-    if os.path.exists(file_path):
-        # If the file exists, send a 200 status code and the contents of the file
-        conn.send('HTTP/1.1 200 OK\n'.encode())
-        with open(file_path, 'r') as f:
-            contents = f.read()
-        conn.send(contents.encode())
-    else:
-        # If the file does not exist, send a 404 status code and an error message
-        conn.send('HTTP/1.1 404 Not Found\n'.encode())
-        conn.send('File not found'.encode())
+    # Accept a new connection
+    connection, address = server_socket.accept()
+    print(f'New connection from {address}')
+    
+    # Receive the request data
+    request_data = connection.recv(BUFFER_SIZE).decode()
+    print(f'Received request:\n{request_data}')
+    
+    # Parse the request
+    request_lines = request_data.split('\n')
+    request_line = request_lines[0]
+    method, path, _ = request_line.split()
+    
+    # Check if the method is GET
+    if method == 'GET':
+        # Get the requested file path
+        file_path = path[1:]
+        
+        # Check if the file exists
+        if os.path.exists(file_path):
+            # Open the file in binary mode
+            with open(file_path, 'rb') as f:
+                # Get the file's mimetype
+                mimetype, _ = mimetypes.guess_type(file_path)
+                # Set the response status to 200 (OK)
+                response_status = 'HTTP/1.1 200 OK\n'
+                # Set the content type header
+                response_headers = f'Content-Type: {mimetype}\n'
+                # Set the content length header
+                response_headers += f'Content-Length: {os.path.getsize(file_path)}\n'
+                # End the headers
+                response_headers += '\n'
+                # Send the response
+                connection.send(response_status.encode())
+                connection.send(response_headers.encode())
+                # Send the file contents to the client
+                connection.sendfile(f)
+        else:
+            # Set the response status to 404 (Not Found)
+            response_status = 'HTTP/1.1 404 Not Found\n'
+            # End the headers
+            response_headers = '\n'
+            # Send the response
+            connection.send(response_status.encode())
+            connection.send(response_headers.encode())
+    
     # Close the connection
-    conn.close()
+    connection.close()
